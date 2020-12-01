0bed7052507affb525baa0a5ceef64ecd7a3c551
PREMINDA <premindabandara0777@gmail.com>
2020-12-02 02:06:25 +0530

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ messageone.textContent = ''
 Formwe.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+    fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
